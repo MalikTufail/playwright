5c362bfede0bd67ab3df2d632e6253144fefd51d
Malik <tufailuni@gmail.com>
2022-12-16 16:58:00 +0500

    upd

--- tests/login.test.js
+++ tests/login.test.js
@@ -2,9 +2,7 @@
 const { chromium,test, expect } = require('@playwright/test');
 test.describe('Login functionality', () => {
   test.beforeEach(async() => {
-    const window = await chromium.launch({
-      headless: true,
-    });
+    const window = await chromium.launch();
     const context= await window.newContext();
     const page=await context.newPage()
     await page.goto('https://ecommerce-playground.lambdatest.io/');
@@ -15,18 +13,14 @@ test.describe('Login functionality', () => {
     await expect(page).toHaveTitle("Account Login")
   })
   test('Navigate to Login', async () => {
-  const window = await chromium.launch({
-    headless: true,
-  });
+  const window = await chromium.launch();
   const context= await window.newContext();
   const page=await context.newPage()
   await expect(page).toHaveTitle("Account Login")
   })
 
   test('Verify that the user is unable to login by providing the invalid Email and Password', async () => {
-    const window = await chromium.launch({
-      headless: true,
-    });
+    const window = await chromium.launch();
     const context = await window.newContext();
     const page = await context.newPage()
     const email = page.getByPlaceholder("input[name='email']")

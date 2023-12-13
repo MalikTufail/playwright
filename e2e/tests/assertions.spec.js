5ef52f8eacad6ab925f8d7839774bf1718e7aa4c
MalikTufail <tufailuni@gmail.com>
2023-12-13 17:19:43 +0500

    pushed latest changes

--- e2e/tests/assertions.spec.js
+++ e2e/tests/assertions.spec.js
@@ -3,7 +3,7 @@ import {test, expect} from '@playwright/test'
 test('@smoke assertions demo', async ({page}) => {
     await page.goto('https://kitchen.applitools.com/')
     await expect(page.locator('text=The Kitchen')).toHaveCount(1)
-    expect(page.locator('#__next > div > div > section > div > h1')).toHaveText('The Kitchen')
+    await expect(page.locator('#__next > div > div > section > div > h1')).toHaveText('The Kitchen')
 
     if( await page.$('text=The Kitchen')) {
         await page.locator('text=The Kitchen').click()
